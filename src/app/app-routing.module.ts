e30eb52c9f3922f3188d0a2a62a425d78a28c43f
Sandra Rodriguez <sandra.latinosoft@gmail.com>
2019-09-17 00:05:24 +0200

    router configution

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent, AboutComponent, ProductoComponent, DocsComponent, ContactComponent } from './components/index.pages';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'producto', component: ProductoComponent },
+  { path: 'documents', component: DocsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', pathMatch: 'full', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/index.pages.ts
@@ -0,0 +1,6 @@
+
+export { AboutComponent } from "./about/about.component";
+export { PortafolioComponent } from "./portafolio/portafolio.component";
+export { ContactComponent } from "./contact/contact.component"; 
+export { DocsComponent } from "./docs/docs.component"; 
+export { ProductoComponent } from "./producto/producto.component"; 
